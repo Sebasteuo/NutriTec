47f3fa66879d81d4db8ede1420cbf0a4d35db3e9
gabriel conejo <gacova95@gmail.com>
2021-11-08 21:27:01 -0600

    creacion del sp login
    store procedure para el login de nutricionistas y clientes

---
+++ Script SQL/storeProcedures-view-triggers/up_LoginCheck.sql
@@ -0,0 +1,46 @@
+/************************************************************************************************
+   Nombre:    up_LoginCheck     
+  
+   Proposito: Revisa las si existe usuario registrado 
+   
+   REVISIONES:
+   Ver          Fecha           Autor                       Descripcion
+   ---------    --------        --------------              ------------------------
+   1.0          8/11/2021       Gabriel conejo valerio      1. Creacion de procedimiento			
+
+
+   Notas: para selecionarlo la tabla las opciones son 'nutricionista' y 'cliente' los resultados son 
+   1 si existe 
+   0 no existe
+
+
+*************************************************************************************************/
+
+CREATE PROCEDURE up_LoginCheck(
+    @identificacion INT,
+	@resultado INT OUTPUT,
+    @StatementType NVARCHAR(50) = '')
+    AS
+  BEGIN
+  DECLARE @esta AS INT;
+  IF @StatementType = 'nutricionista'
+        BEGIN
+            IF EXISTS ( select CodigoNutricionista from dbo.NUTRICIONISTA where CodigoNutricionista = @identificacion )
+                BEGIN
+                    set @resultado =1
+                END
+            ELSE
+                    set @resultado =0
+
+        END
+        
+        IF @StatementType = 'cliente'
+        BEGIN
+           IF EXISTS ( select Cedula from dbo.USUARIO where Cedula =  @identificacion)
+                BEGIN
+                    set @resultado =1
+                END
+                ELSE
+                    set @resultado =0
+        END
+  END
