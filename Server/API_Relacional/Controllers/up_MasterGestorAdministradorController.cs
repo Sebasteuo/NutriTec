94e55f8526af4461658ce40751804657aab09d7c
Bojorge <38023924+Bojorge@users.noreply.github.com>
2021-11-13 11:40:33 -0600

    Implementación de procedimientos, vistas y funciones

---
+++ Server/API_Relacional/Controllers/up_MasterGestorAdministradorController.cs
@@ -0,0 +1,73 @@
+﻿using API_Relacional.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Relacional.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class up_MasterGestorAdministradorController : ControllerBase
+    {
+        private string cadenaDeConexion = "SQLServerConnection"; //hace referencia a la cadena de conexion en appsettings.json
+        private readonly IConfiguration _configuration;
+
+        //el metodo constructor recibe como parametro una instancia de la interface Iconfiguration que permite la representacion de un conjunto de propiedades clave/valor
+        public up_MasterGestorAdministradorController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        // POST api/<HablaController>
+        [HttpPost]
+        public JsonResult Post(Administrador x, string statementtype)
+        {
+            string query = @"
+                exec dbo.up_MasterGestorAdministrador @adminid, @correo, @password, @statementtype
+                ";
+
+            String result;
+            DataTable table = new DataTable();
+            SqlDataReader reader;
+
+            string sqlDataSource = _configuration.GetConnectionString(cadenaDeConexion);//La fuente de los datos se obtiene de la cadena de conexion
+
+            using (SqlConnection connection = new SqlConnection(sqlDataSource))//Se utiliza la fuente de datos para hacer la conexion
+            {
+                connection.Open();
+
+                using (SqlCommand cmd = new SqlCommand(query, connection))//El comando a ejecutar se hace con un query y la conexion
+                {
+
+                    //Se agregan los valores y el tipo de dato respectivo
+                    cmd.Parameters.Add("@adminid", SqlDbType.Int);
+                    cmd.Parameters["@adminid"].Value = x.adminid;
+
+                    cmd.Parameters.Add("@correo", SqlDbType.Int);
+                    cmd.Parameters["@correo"].Value = x.correo;
+
+                    cmd.Parameters.Add("@password", SqlDbType.Int);
+                    cmd.Parameters["@password"].Value = x.password;
+
+                    cmd.Parameters.Add("@statementtype", SqlDbType.NVarChar);
+                    cmd.Parameters["@statementtype"].Value = statementtype;
+
+                    reader = cmd.ExecuteReader(); //El lector ejecuta el comando
+                    table.Load(reader); //El objeto DataTable se carga con los datos del lector
+                    result = JsonConvert.SerializeObject(table); //Se serializa la tabla a JSON
+                    reader.Close(); //Se cierra  el lector
+                    connection.Close(); //Se cierra la conexion
+                }
+            }
+            return new JsonResult(result);
+        }
+    }
+}
